9662af63bbb91a48d0667464dcdc6dfd6bef7496
kimijin8134 <davidjin8134@gmail.com>
2017-08-19 11:13:41 -0700

    UI Fix
    TextField UI fix

--- WindowShopper/WindowShopper/Controller/MainVC.swift
+++ WindowShopper/WindowShopper/Controller/MainVC.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var WageText: CurrencyTextField!
+    
+    @IBOutlet weak var PriceText: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let calculateButton = UIButton(frame: CGRect(x:0,y:0,width:view.frame.size.width,height:60))
+        calculateButton.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        calculateButton.setTitle("Calculate", for: .normal)
+        calculateButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calculateButton.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        WageText.inputAccessoryView = calculateButton
+        PriceText.inputAccessoryView = calculateButton
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func calculate(){
+        
     }
-
-
+    
+    
 }
 
--- WindowShopper/WindowShopper/View/CurrencyTextField.swift
+++ WindowShopper/WindowShopper/View/CurrencyTextField.swift
@@ -26,6 +26,7 @@ class CurrencyTextField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+        
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor.white])
             attributedPlaceholder = place
