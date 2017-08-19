65926c02fc814b966b39e188f27222f3fd3cc7b2
kimijin8134 <davidjin8134@gmail.com>
2017-08-18 20:39:50 -0700

    ui

---
+++ WindowShopper/WindowShopper/View/CurrencyTextField.swift
@@ -0,0 +1,36 @@
+//
+//  CurrencyTextField.swift
+//  WindowShopper
+//
+//  Created by Wei Jin on 2017-08-18.
+//  Copyright © 2017 Wei Jin. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        customizeView()
+        
+    }
+    
+    func customizeView(){
+        backgroundColor = #colorLiteral(red: 0.9961721301, green: 0.9902489781, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor:UIColor.white])
+            attributedPlaceholder = place
+            textColor = UIColor.white
+        }
+    }
+    
+}
