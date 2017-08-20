fc79a84f5b976a5cfa1ee97608b8783cf042700d
kimijin8134 <davidjin8134@gmail.com>
2017-08-20 15:24:05 -0700

    Dicee
    Completed

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        randomDices()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDices()
+    }
+    
+    func randomDices(){
+        randomDiceIndex1 = Int (arc4random_uniform(5)) + 1
+        randomDiceIndex2 = Int (arc4random_uniform(5)) + 1
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2)")
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        randomDices()
 
+    }
+    
 }
 
