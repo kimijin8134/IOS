4c68197aa3418b0d2d82fa4c5e5ffd8ab036a020
kimijin8134 <davidjin8134@gmail.com>
2017-08-23 21:52:31 -0700

    Added pods

--- Clima-iOS11-master/Clima/WeatherViewController.swift
+++ Clima-iOS11-master/Clima/WeatherViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
@@ -17,7 +18,7 @@ class WeatherViewController: UIViewController {
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,6 +32,10 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     
         
         
@@ -75,12 +80,23 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count-1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            let latitude = String (location.coordinate.latitude)
+            let longitude = String (location.coordinate.longitude)
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
 
     
